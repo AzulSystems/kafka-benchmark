ce8f77ee6215b1e625194fd6a549f73ac5b7884f
Ruslan <novosibman@gmail.com>
2022-07-21 20:35:03 +0700

    Simplified build.sh script

--- build.sh
+++ build.sh
@@ -32,19 +32,10 @@
 
 KAFKA_BASE_DIR=$(cd $(dirname $0); pwd)
 
-#bash "${KAFKA_BASE_DIR}/../tussle-framework/build.sh" || exit 1
+bash "${KAFKA_BASE_DIR}/../tussle-framework/build.sh" || exit 1
 
 cd "${KAFKA_BASE_DIR}" || exit 1
 echo "Building $(pwd)..."
 
-rm -v *.jar
-
 mvn clean
-mvn package -DskipTests || exit 1
-
-from=$(find target -name *-jar-with-dependencies.jar)
-to=${from/-jar-with-dependencies/}
-to=${to##*/}
-
-cp -fv ${from} ${to}
-chmod 777 ${to}
+mvn package -DskipTests
